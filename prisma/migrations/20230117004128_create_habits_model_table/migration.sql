150e08e06194b4e7e8b912fccf9361fe96d1a0d1
Wender Barbosa <wenblack012@gmail.com>
2023-01-16 22:23:19 -0300

    Created initial route and configure cor

---
+++ prisma/migrations/20230117004128_create_habits_model_table/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "creted_at" DATETIME NOT NULL
+);
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log('novo teste')
+import Fastify from "fastify";
+import cors from  '@fastify/cors'
+import {PrismaClient} from'@prisma/client'
+
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async ()=>{
+  const habits =  await prisma.habit.findMany()
+  return habits
+})
+ 
+app.listen({
+  port:3333
+}).then(()=>{
+  console.log('Server runnig on port 3333!')
+})
