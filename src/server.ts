f8df4177bb3521291ddcb60a0abddcaa656a290d
Wender Barbosa <wenblack012@gmail.com>
2023-01-17 22:18:56 -0300

    Created seed for database

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
-import Fastify from "fastify";
-import cors from  '@fastify/cors'
-import {PrismaClient} from'@prisma/client'
+import Fastify from "fastify";//Route module
+import cors from  '@fastify/cors'//Access module(Allow third party apps/front end ) 
+import {PrismaClient} from'@prisma/client'//ORM
 
-
+//Starting Modules
 const app = Fastify()
 const prisma = new PrismaClient()
 
+//Declaring routes
 app.register(cors)
-
 app.get('/', async ()=>{
   const habits =  await prisma.habit.findMany()
   return habits
 })
- 
-app.listen({
+
+app.listen({//Ports settings
   port:3333
 }).then(()=>{
-  console.log('Server runnig on port 3333!')
+  console.log('Server running on port 3333!')
 })
