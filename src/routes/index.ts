6c6c1a3295042785cd03b7affb2fa81200048169
Wender Barbosa <wenblack012@gmail.com>
2023-01-18 21:17:11 -0300

    Created routes,lib and first post action

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,35 @@
 import {FastifyInstance} from "fastify"
 import { prisma } from "../lib/prisma"
+import {number, z} from 'zod'//library that helps  u typing and validating data
+import dayjs from "dayjs"// Library that helps u work with data
 
 export async function appRoutes (app : FastifyInstance) {
-  app.get('/', async ()=>{
-    const habits =  await prisma.habit.findMany()
-    return habits
+  app.post('/habits', async (request)=>{
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+        z.number().min(0).max(6)
+      )
+    })
+    const {title, weekDays} = createHabitBody.parse(request.body) 
+    //startOf = wipe minutes and hours for new date
+    //toDate() = Convert string value to date
+    const today = dayjs().startOf('day').toDate()
+
+
+    await prisma.habit.create({
+      data:{
+        title: title,
+        created_at: today,
+        WeekDays:{
+          create: weekDays.map(weekDay=>{
+            return {
+              week_day:weekDay
+            }
+          })
+        }
+      }
+    })
   })
   
 }
