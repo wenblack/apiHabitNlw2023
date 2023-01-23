9ddc2ff19ab162bb09e69210ce37a916bffdb649
Wender Barbosa <wenblack012@gmail.com>
2023-01-22 23:31:52 -0300

    Altered port number

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import Fastify from 'fastify'
+import * as dotenv from 'dotenv'
 import cors from '@fastify/cors'
 import { appRoutes } from "./routes"
 
+dotenv.config()
 const app = Fastify()
-
+const ports = Number(process.env.PORT)
 app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-  port: 3333,
+  port: ports || 3000,
 }).then(() => {
-  console.log('HTTP Server running!')
+  console.log('HTTP Server running on port '+ports)
 })
